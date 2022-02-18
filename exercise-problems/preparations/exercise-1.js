cfe8073a17b764e65a776c23ef40eff3c96cf634
Ricardo Landeros <landerosr81@gmail.com>
2022-02-17 18:41:55 -0800

    foo.js

---
+++ exercise-problems/preparations/exercise-1.js
@@ -0,0 +1,3 @@
+/* Create a directory named my_folder and navigate to that directory. Create two files named one.js and two.js in my_folder. Add the following JavaScript code to one.js: 
+console.log("this is file one");
+Add some code to two.js that logs 'this is file two' */
