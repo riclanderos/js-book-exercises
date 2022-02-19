c5f6e34df9c2c324c6ea0c652597eca35e27210d
Ricardo Landeros <landerosr81@gmail.com>
2022-02-18 20:25:10 -0800

    basics exercise 8

---
+++ exercise-problems/basics/exercise-8.js
@@ -0,0 +1,12 @@
+/* Create an array named names that contains a list of pet names. For instance:
+
+Name
+asta
+butterscotch
+pudding
+neptune
+darwin
+You can use that data or make up your own. */
+
+let names = ["asta", "butterscotch", "pudding", "neptune", "darwin", "alfalfa"];
+console.log(names);
