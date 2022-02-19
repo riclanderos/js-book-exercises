068d4e727958f73ef5a1a278a04688c94b1e49e3
Ricardo Landeros <landerosr81@gmail.com>
2022-02-18 20:03:46 -0800

    basics exercise 4

---
+++ exercise-problems/basics/exercise-4.js
@@ -0,0 +1,3 @@
+//Explain why this code logs '510' instead of 15.
+console.log("5" + 10);
+// The code will coerce the 10 number value into a string since the "5" is a string. It will then concatenate (join) the two values into '510'.
