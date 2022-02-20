d8438dac701a52290822ed13862d56647e4d38e7
Ricardo Landeros <landerosr81@gmail.com>
2022-02-19 16:45:10 -0800

    basics exercise 11

---
+++ exercise-problems/basics/exercise-11.js
@@ -0,0 +1,4 @@
+// What value does the following expression evaluate to?
+parseInt("3.1415");
+
+// It will evaluate to the number 3. parseInt will convert a string beginning with a number to a number. It will stop converting once there are no more numbers or if there is a period.
