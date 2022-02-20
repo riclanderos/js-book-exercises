30bab48b3de01fb5e937b7eb3bd4882ca0cd032e
Ricardo Landeros <landerosr81@gmail.com>
2022-02-19 16:37:04 -0800

    basics exercise 9

---
+++ exercise-problems/basics/exercise-9.js
@@ -0,0 +1,20 @@
+/* Create an object named pets that contains a list of pet names and the type of animal. For instance:
+
+Name	Animal
+asta	dog
+butterscotch	cat
+pudding	cat
+neptune	fish
+darwin	lizard
+You can use that data or make up your own. */
+
+let pets = {
+  asta: "dog",
+  butterscotch: "cat",
+  pudding: "cat",
+  neptune: "fish",
+  darwin: "lizard",
+  poe: "cat",
+};
+
+console.log(pets);
