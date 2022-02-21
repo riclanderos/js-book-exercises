6db822a859406cc186c8bc483142fb6afdbee304
Ricardo Landeros <landerosr81@gmail.com>
2022-02-20 17:16:57 -0800

    variables  exercise 1- 3

---
+++ exercise-problems/variables/greeter.js
@@ -0,0 +1,18 @@
+/* Exercise-1
+Write a program named greeter.js that greets 'Victor' three times. Your 
+program should use a variable and not hard code the string value 'Victor' 
+in each greeting. Here's an example run of the program: */
+
+let firstName = "Victor";
+console.log("Good Morning, " + firstName + ".");
+console.log("Good Afternoon, " + firstName + ".");
+console.log("Good Evening, " + firstName + ".");
+
+/* Practice doing it with for loop
+
+let timeOfDay = ["Morning", "Afternoon", "Evening"];
+for (i = 2; i < timeOfDay.length; i++) {
+  console.log(`Good ${timeOfDay[0]}, ${firstName}.`);
+  console.log(`Good ${timeOfDay[1]}, ${firstName}.`);
+  console.log(`Good ${timeOfDay[2]}, ${firstName}.`);
+} */
