0dca00759ae3e25f3442749f248ddb27dcb681dc
Ricardo Landeros <landerosr81@gmail.com>
2022-02-24 18:58:08 -0800

    exercise 5.js

---
+++ exercise-problems/functions/exercise-5.js
@@ -0,0 +1,9 @@
+// What does the following code log to the console?
+
+function scream(words) {
+  return words + "!!!!";
+}
+
+scream("Yipeee");
+
+// It doens't log anything to the console because there is no console.log.
