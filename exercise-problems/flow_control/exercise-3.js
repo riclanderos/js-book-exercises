db5f069668fc2c740c36f68e3e0b049be1985db9
Ricardo Landeros <landerosr81@gmail.com>
2022-02-26 11:57:43 -0800

    exercise 3.js

---
+++ exercise-problems/flow_control/exercise-3.js
@@ -0,0 +1,15 @@
+/* Let's improve our previous implementation of evenOrOdd. Add a validation check 
+to ensure that the argument is an integer. If it isn't, the function should issue 
+an error message and return. */
+
+function evenOrOdd(number) {
+  if (!Number.isInteger(number)) {
+    console.log("Sorry, the value you passed is not an integer");
+    return;
+  }
+  if (number % 2 === 0) {
+    console.log("even");
+  } else {
+    console.log("odd");
+  }
+}
