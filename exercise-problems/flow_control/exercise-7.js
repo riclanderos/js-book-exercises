8d769f6362b62f20a5ac08753fc3c8f5183160b3
Ricardo Landeros <landerosr81@gmail.com>
2022-02-26 16:23:25 -0800

    exercise 7.js

---
+++ exercise-problems/flow_control/exercise-7.js
@@ -0,0 +1,15 @@
+/* Write a function that takes a string as an argument and returns an all-caps 
+version of the string when the string is longer than 10 characters. Otherwise, it 
+should return the original string. Example: change 'hello world' to 'HELLO WORLD', 
+but don't change 'goodbye'. */
+
+function aString(string) {
+  if (string.length > 10) {
+    return string.toUpperCase();
+  } else {
+    return string;
+  }
+}
+
+console.log(aString("hello world"));
+console.log(aString("goodbye"));
