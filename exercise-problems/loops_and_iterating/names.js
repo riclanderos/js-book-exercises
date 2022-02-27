e43adcfdc2dffeded702a163a98ca82f21560b74
Ricardo Landeros <landerosr81@gmail.com>
2022-02-26 18:33:46 -0800

    loops control exercise names.js

--- exercise-problems/loops_and_iterating/names.js
+++ exercise-problems/loops_and_iterating/names.js
@@ -1,3 +1,4 @@
+/* written using a while loop
 let names = ["Chris", "Kevin", "Naveed", "Pete", "Victor"];
 let upperNames = [];
 let index = 0;
@@ -5,7 +6,35 @@ let index = 0;
 while (index < names.length) {
   let upperCaseName = names[index].toUpperCase();
   upperNames.push(upperCaseName);
-  index++;
+  index += 1;
+}
+
+console.log(upperNames); 
+
+ // rewritten using a for loop
+
+let names = ['Chris', 'Kevin', 'Naveed', 'Pete', 'Victor'];
+let upperNames = [];
+
+for (let index = 0; index < names.length; index += 1) {
+  let upperCaseName = names[index].toUpperCase();
+  upperNames.push(upperCaseName);
+}
+
+console.log(upperNames); */
+
+// controlling loops using continue
+
+let names = ["Chris", "Kevin", "Naveed", "Pete", "Victor"];
+let upperNames = [];
+
+for (let index = 0; index < names.length; index += 1) {
+  if (names[index] === "Naveed") {
+    continue;
+  }
+
+  let upperCaseName = names[index].toUpperCase();
+  upperNames.push(upperCaseName);
 }
 
 console.log(upperNames);
